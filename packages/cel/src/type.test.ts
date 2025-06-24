12b68f11cecc0bfae2417314255165c18d42c14e
Sri Krishna <skrishna@buf.build>
2025-06-24 16:52:13 -0400

    Switch to expect-type

--- packages/cel/src/type.test.ts
+++ packages/cel/src/type.test.ts
@@ -21,31 +21,29 @@ import {
   type TupleTypeOf,
 } from "./type.js";
 import { CelList, CelMap, CelUint } from "./value/value.js";
-import { NATIVE_ADAPTER } from "./adapter/native.js";
-import * as type from "./value/type.js";
-import { TimestampSchema } from "@bufbuild/protobuf/wkt";
-import { create } from "@bufbuild/protobuf";
+import { type Timestamp, TimestampSchema } from "@bufbuild/protobuf/wkt";
+import { expectTypeOf } from "expect-type";
 
 test("TypeOf", () => {
-  1n satisfies TypeOf<CelScalar.INT>;
-  "" satisfies TypeOf<CelScalar.STRING>;
-  false satisfies TypeOf<CelScalar.BOOL>;
-  1.1 satisfies TypeOf<CelScalar.DOUBLE>;
-  new CelUint(123n) satisfies TypeOf<CelScalar.UINT>;
-  new Uint8Array([]) satisfies TypeOf<CelScalar.BYTES>;
-  null satisfies TypeOf<CelScalar.NULL>;
+  expectTypeOf<TypeOf<CelScalar.INT>>().toEqualTypeOf<bigint>();
+  expectTypeOf<TypeOf<CelScalar.STRING>>().toEqualTypeOf<string>();
+  expectTypeOf<TypeOf<CelScalar.BOOL>>().toEqualTypeOf<boolean>();
+  expectTypeOf<TypeOf<CelScalar.DOUBLE>>().toEqualTypeOf<number>();
+  expectTypeOf<TypeOf<CelScalar.UINT>>().toEqualTypeOf<CelUint>();
+  expectTypeOf<TypeOf<CelScalar.BYTES>>().toEqualTypeOf<Uint8Array>();
+  expectTypeOf<TypeOf<CelScalar.NULL>>().toEqualTypeOf<null>();
+  expectTypeOf<TypeOf<CelScalar.INT>>().toEqualTypeOf<bigint>();
+  expectTypeOf<TypeOf<CelScalar.INT>>().toEqualTypeOf<bigint>();
   const list = listType(CelScalar.BOOL);
-  new CelList([], NATIVE_ADAPTER, type.LIST) satisfies TypeOf<typeof list>;
+  expectTypeOf<TypeOf<typeof list>>().toEqualTypeOf<CelList>();
   const map = mapType(CelScalar.STRING, CelScalar.STRING);
-  new CelMap(
-    new Map<string, string>(),
-    NATIVE_ADAPTER,
-    new type.MapType(type.STRING, type.STRING),
-  ) satisfies TypeOf<typeof map>;
-  create(TimestampSchema) satisfies TypeOf<typeof TimestampSchema>;
+  expectTypeOf<TypeOf<typeof map>>().toEqualTypeOf<CelMap<string, string>>();
+  expectTypeOf<TypeOf<typeof TimestampSchema>>().toEqualTypeOf<Timestamp>();
 });
 
 test("TupleTypeOf", () => {
-  ["", 1n] satisfies TupleTypeOf<[CelScalar.STRING, CelScalar.INT]>;
-  [""] satisfies TupleTypeOf<[CelScalar.STRING]>;
+  expectTypeOf<TupleTypeOf<[CelScalar.STRING, CelScalar.INT]>>().toEqualTypeOf<
+    [string, bigint]
+  >();
+  expectTypeOf<TupleTypeOf<[CelScalar.STRING]>>().toEqualTypeOf<[string]>();
 });
